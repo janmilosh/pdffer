65eb02d1974debec1e5df43e695c4e247a7340cd
janmilosh <janmilosh@gmail.com>
2015-02-25 07:48:37 -0500

    Start beautiful soup methods

--- app/get_links.py
+++ app/get_links.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 import os 
+
+from bs4 import BeautifulSoup
 import requests
 
 class FormLinks:
-	# def __init__(self):
- #        self.root_dir = os.getcwd()
- #        self.form_links = []
+    def __init__(self):
+        self.root_dir = os.getcwd()
+        self.form_links = []
     
-    def _get_links(url):
+    def _get_links_page(url):
         return requests.get(url)
+
+    def _make_soup(text):
+        soup = BeautifulSoup(text)
+        print(soup.prettify())
+        return soup
 
 
 
--- tests/small/test_get_links.py
+++ tests/small/test_get_links.py
@@ -8,7 +8,7 @@ class ExpressScriptsLinksTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         url = 'https://www.express-scripts.com/services/physicians/medcopa/index.shtml'
-        cls.response = FormLinks._get_links(url)
+        cls.response = FormLinks._get_links_page(url)
 
     def test_makes_successful_get_request_to_express_scripts(cls):
         cls.assertEqual(cls.response.status_code, 200)
@@ -16,8 +16,10 @@ class ExpressScriptsLinksTest(unittest.TestCase):
     def test_get_request_returns_a_webpage(cls):
         cls.assertIn(b'!DOCTYPE html', cls.response.content)
 
-    def test_saves_content_to_pickled_file(cls)
-        
+    def test_make_soup_from_page(cls):
+        soup_object = FormLinks._make_soup(cls.response.text)
+        cls.assertIn(u'!DOCTYPE html', soup_object)
+
 
 if __name__ == '__main__':
     unittest.main()
